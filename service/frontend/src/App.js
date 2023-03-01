2df9c40041fff67c30f4094a7d428a31b0715dd7
Donvik1976 <vdonets76@mail.ru>
2023-03-01 12:03:30 +0600

    lesson_2 Got data about all users and displayed them on the page

--- service/frontend/src/App.js
+++ service/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 import UsersList from "./components/Users.js";
@@ -11,25 +12,27 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const users = [
-            {
-                "username": "TimRus",
-                "first_name": "Тимофей",
-                "last_name": "Руснак",
-                "email": "timofey21@gmail.com"
-            },
-            {
-                "username": "anna",
-                "first_name": "Анна",
-                "last_name": "Расторгуева",
-                "email": "anna1971@hotmail.com"
-            }
-        ]
-        this.setState(
-            {
-                'users':users
-            }
-        )
+        // const users = [
+        //     {
+        //         "username": "TimRus",
+        //         "first_name": "Тимофей",
+        //         "last_name": "Руснак",
+        //         "email": "timofey21@gmail.com"
+        //     },
+        //     {
+        //         "username": "anna",
+        //         "first_name": "Анна",
+        //         "last_name": "Расторгуева",
+        //         "email": "anna1971@hotmail.com"
+        //     }
+        // ]
+        axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+            this.setState(
+                {
+                    'users':response.data
+                }
+            )}).catch(error => console.log(error))
+
     }
 
     render() {
