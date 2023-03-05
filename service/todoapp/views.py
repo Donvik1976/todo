c39193b71ebdc4bc64d27da08a61d85bca31b2f9
Donvik1976 <vdonets76@mail.ru>
2023-03-05 22:51:07 +0600

    lesson_4 Set page size for pagination

--- service/todoapp/views.py
+++ service/todoapp/views.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 
@@ -6,11 +7,20 @@ from .models import Project, TODO
 from .serializers import ProjectModelSerializer, TODOModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     filterset_class = ProjectFilter
+    pagination_class = ProjectLimitOffsetPagination
+
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class TODOModelViewSet(ModelViewSet):
@@ -18,4 +28,5 @@ class TODOModelViewSet(ModelViewSet):
     serializer_class = TODOModelSerializer
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     filterset_fields = ['project']
+    pagination_class = TODOLimitOffsetPagination
 
